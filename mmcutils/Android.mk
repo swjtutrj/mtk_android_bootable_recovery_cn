74c33f7afa416baaa579743123b3e6bea4ac5f89
zengwenhao <doyoulove0@aliyun.com>
2014-09-12 15:32:38 +0800

    Update Android.mk

--- mmcutils/Android.mk
+++ mmcutils/Android.mk
@@ -3,8 +3,14 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+
+ifeq ($(BOARD_RECOVERY_MTK));true
+LOCAL_SRC_FILES := \
+	mmcutils_mtk.c
+else
 LOCAL_SRC_FILES := \
 	mmcutils.c
+endif
 
 LOCAL_MODULE := libmmcutils
 LOCAL_MODULE_TAGS := eng
